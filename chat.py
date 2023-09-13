6dbbef85191423075147abb0456e6add12e1d31a
Ksenia <klamskaa@gmail.com>
2023-09-13 12:05:10 +0300

    six

--- chat.py
+++ chat.py
@@ -16,6 +16,11 @@ def divide_fractions(num1, den1, num2, den2):
     result = Fraction(num1, den1) / Fraction(num2, den2)
     return result.numerator, result.denominator
 
+def main():
+    num1 = int(input("Введите числитель первой дроби: "))
+    den1 = int(input("Введите знаменатель первой дроби: "))
+    num2 = int(input("Введите числитель второй дроби: "))
+    den2 = int(input("Введите знаменатель второй дроби: "))
 
 print("Сложение дробей: ", add_fractions(num1, den1, num2, den2))
 print("Вычитание дробей: ", subtract_fractions(num1, den1, num2, den2))
