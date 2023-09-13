81cfa7cf382fd306cf5c5256c78ac63ed6547883
Ksenia <klamskaa@gmail.com>
2023-09-13 11:56:59 +0300

    second

--- chat.py
+++ chat.py
@@ -8,6 +8,6 @@ def subtract_fractions(num1, den1, num2, den2):
     result = Fraction(num1, den1) - Fraction(num2, den2)
     return result.numerator, result.denominator
 print("Сложение дробей: ", add_fractions(num1, den1, num2, den2))
-
+print("Вычитание дробей: ", subtract_fractions(num1, den1, num2, den2)
 if name == "main":
     main()
