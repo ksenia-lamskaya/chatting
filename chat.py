6624a82f7c241e682d524f631f56d8ad84c219ce
Ksenia <klamskaa@gmail.com>
2023-09-13 12:04:10 +0300

    five

--- chat.py
+++ chat.py
@@ -12,6 +12,10 @@ def multiply_fractions(num1, den1, num2, den2):
     result = Fraction(num1, den1) * Fraction(num2, den2)
     return result.numerator, result.denominator
 
+def divide_fractions(num1, den1, num2, den2):
+    result = Fraction(num1, den1) / Fraction(num2, den2)
+    return result.numerator, result.denominator
+
 
 print("Сложение дробей: ", add_fractions(num1, den1, num2, den2))
 print("Вычитание дробей: ", subtract_fractions(num1, den1, num2, den2))
