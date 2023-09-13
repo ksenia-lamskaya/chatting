385545bf76191b38f2acf9add52027cd3cbfc912
Ksenia <klamskaa@gmail.com>
2023-09-13 12:01:46 +0300

    fours

--- chat.py
+++ chat.py
@@ -12,7 +12,10 @@ def multiply_fractions(num1, den1, num2, den2):
     result = Fraction(num1, den1) * Fraction(num2, den2)
     return result.numerator, result.denominator
 
+
 print("Сложение дробей: ", add_fractions(num1, den1, num2, den2))
 print("Вычитание дробей: ", subtract_fractions(num1, den1, num2, den2))
+print("Умножение дробей: ", multiply_fractions(num1, den1, num2, den2))
+print("Деление дробей: ", divide_fractions(num1, den1, num2, den2))
 if name == "main":
     main()
